8214a02dcc8e40b68548e8da981f92de0cd0b683
Belowkm <belowkm@163.com>
2024-06-08 01:35:42 +0800

    WeBase+智能合约全局提交

---
+++ Contract/Town_Gov.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.25;
+pragma experimental ABIEncoderV2;
+
+import "./Government.sol";
+
+contract Town_Gov is Government {
+    constructor(string name) public Government(name) {}
+}
